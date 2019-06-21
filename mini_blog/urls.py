5eb8619b0311019197172d84ca9b246ca06d064d
Andy Dearing <andy.dearing@gmail.com>
2019-06-21 11:59:12 -0400

    skeleton page up and running

--- mini_blog/urls.py
+++ mini_blog/urls.py
@@ -19,3 +19,23 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+# Use include() to add paths from the blog application 
+from django.urls import include
+from django.urls import path
+
+urlpatterns += [
+    path('blog/', include('blog.urls')),
+]
+
+#Add URL maps to redirect the base URL to our application
+from django.views.generic import RedirectView
+urlpatterns += [
+    path('', RedirectView.as_view(url='/blog/', permanent=True)),
+]
+
+# Use static() to add url mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
